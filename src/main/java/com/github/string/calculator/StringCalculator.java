ffe3416ce9a28dca305f3780c78e235dfe389176
xushanchuan <aisensiy@163.com>
2015-10-12 17:36:54 +0800

    for single char

--- src/main/java/com/github/string/calculator/StringCalculator.java
+++ src/main/java/com/github/string/calculator/StringCalculator.java
@@ -2,9 +2,6 @@ package com.github.string.calculator;
 
 public class StringCalculator {
     public static int sum(String input) {
-        if (input.equals("1")) {
-            return 1;
-        }
-        return 0;
+        return Integer.parseInt(input);
     }
 }
