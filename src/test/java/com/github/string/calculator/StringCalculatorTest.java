d38a17f777d21e7b57b7afea25046881597ea775
kiwi <jcwen@thoughtworks.com>
2015-10-12 17:29:21 +0800

    add static import for test

--- src/test/java/com/github/string/calculator/StringCalculatorTest.java
+++ src/test/java/com/github/string/calculator/StringCalculatorTest.java
@@ -2,12 +2,18 @@ package com.github.string.calculator;
 
 import org.junit.Test;
 
+import static com.github.string.calculator.StringCalculator.sum;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
     @Test
     public void should_return_0_for_empty() {
-        assertThat(StringCalculator.sum("0"), is(0));
+        assertThat(sum("0"), is(0));
+    }
+
+    @Test
+    public void should_return_same_value_for_single_input() {
+        assertThat(sum("1"), is(1));
     }
 }
