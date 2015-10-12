7c15bb1dff503122d7b11738c9c6df8cc6fae9b6
xushanchuan <aisensiy@163.com>
2015-10-12 17:57:17 +0800

    impl

--- src/main/java/com/github/string/calculator/StringCalculator.java
+++ src/main/java/com/github/string/calculator/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        input = input.replace('\n', ',');
         return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
