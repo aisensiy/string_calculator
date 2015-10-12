6fe6fe337ad6454b8ca1c84382135a18ee75c5f5
xushanchuan <aisensiy@163.com>
2015-10-12 17:47:37 +0800

    for test empty string

--- src/main/java/com/github/string/calculator/StringCalculator.java
+++ src/main/java/com/github/string/calculator/StringCalculator.java
@@ -2,6 +2,9 @@ package com.github.string.calculator;
 
 public class StringCalculator {
     public static int sum(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
         if (input.contains(",")) {
             String[] split = input.split(",");
             return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
